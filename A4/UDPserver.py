5714db0ad80ae51d60d9732f226c7706f286b37d
Z1pp0-a <958711886@qq.com>
2025-06-12 20:27:59 +0800

    Define the UDPFileServer class

--- A4/UDPserver.py
+++ A4/UDPserver.py
@@ -3,3 +3,12 @@ import threading
 import os
 import base64
 import random
+
+class  UDPFileServer:
+    def __init__(self,port):
+        self.port = port
+        self.server_socket = socket.socket(socket.AF_INET_,socket.SOCK_STREAM)
+        self.server_socket.bind('0.0.0.0',self.port)
+        self.server_socket.listen(5)
+        print(f"SErver started on port {self.port}")
+
