67c713f779d18ae82510c74bdfa4a18daa0df797
Z1pp0-a <958711886@qq.com>
2025-06-12 20:47:05 +0800

    Implement the handle_client method to handle client requests

--- A4/UDPserver.py
+++ A4/UDPserver.py
@@ -18,3 +18,23 @@ class  UDPFileServer:
             print(f"New connection from{addr}")
             client_thread = threading.Thread(target=self.handle_client,args=(client_socket,))
             client_thread.start()
+    
+    def handle_client(self,client_socket):
+        try:
+            request = client_socket.recv(1024).decode('utf-8')
+            if request.startwith("DOWNLOAD"):
+                filename = request.split("  ")[1]
+                if os.path.exists(filename):
+                    file_size = os.path.getsize(filename)
+                    udp_port = random.randint(50000,51000)
+                    response = f"OK{filename}SIZE{file_size}PORT{udp_port}"
+                    client_socket.send(response.encode('utf-8'))
+                    self.transfer_file(filename,file_size,udp_port,client_socket.getpeername())
+                else:
+                    response = f"ERR{filename}NOT_FOUND"
+                    client_socket.send(response.encode('utf-8'))
+        except Exception as e:
+            print(f"Error handling client:{e}")
+        finally:
+            client_socket.close()
+            
